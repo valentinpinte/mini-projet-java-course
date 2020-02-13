f5102ea1078547ff326e132733a648256cac2578
Julien MANNECHEZ <julien.mannechez@b02n16lnx.imtld.local>
2020-02-13 14:42:12 +0100

    premier test

---
+++ VecteurTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Asset.*;
+
+import org.junit.Test;
+
+public class VecteurTest {
+
+    @Test 
+    public void monPremierTest() {
+        Vecteur v1_test = new Vecteur(); // test du constructeur vecteur
+
+    }
+}
